b50af1a32f127078b7dabd9e7e20589fc077cd99
Akash Singh <77776282+iakash-singh@users.noreply.github.com>
2024-08-22 11:36:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (8.34%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int findComplement(int num) {
-        long pow2 = 2, temp = num;
-        while(temp>>1){
-            temp >>=1;
-            pow2<<=1;
+        vector<int> temp;
+        while(num!=0){
+            temp.push_back(num%2);
+            num = num/2;
         }
 
-        return pow2-num-1;
+        for(int i=0;i<temp.size();i++){
+            if(temp[i]==1) temp[i] = 0;
+            else{
+                temp[i] = 1;
+            }
+        }
+        int res = 0;
+        for(int i= temp.size()-1;i>-1;i--){
+            res = res*2+temp[i];
+        }
+        return res;
     }
 };

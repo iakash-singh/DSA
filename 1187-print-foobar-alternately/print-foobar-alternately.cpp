015debf2753a618f6f5a335b3b4946c07ea5be6c
Akash Singh <77776282+iakash-singh@users.noreply.github.com>
2024-09-29 22:58:55 +0530

    Time: 3 ms (90.38%) | Memory: 10 MB (94.76%) - LeetSync

--- 1187-print-foobar-alternately/print-foobar-alternately.cpp
+++ 1187-print-foobar-alternately/print-foobar-alternately.cpp
@@ -1,31 +1,40 @@
 class FooBar {
 private:
     int n;
-    atomic<bool> fooTurn;
+    sem_t foosem;
+    sem_t barsem;
 
 public:
     FooBar(int n) {
-        fooTurn = true;
         this->n = n;
+        sem_init(&foosem,0,1);
+        sem_init(&barsem,0,0);
     }
 
     void foo(function<void()> printFoo) {
         
         for (int i = 0; i < n; i++) {
-            while(!fooTurn.load());
+            
+        	// printFoo() outputs "foo". Do not change or remove this line.
+            sem_wait(&foosem);
         	printFoo();
-            fooTurn.store(false);
+            sem_post(&barsem);
         }
+
     }
 
     void bar(function<void()> printBar) {
         
         for (int i = 0; i < n; i++) {
-        	
-            while(fooTurn.load());
+            
+        	// printBar() outputs "bar". Do not change or remove this line.
+            sem_wait(&barsem);
         	printBar();
-            fooTurn.store(true);
-
+            sem_post(&foosem);
         }
+    }
+    ~FooBar(){
+        sem_destroy(&foosem);
+        sem_destroy(&barsem);
     }
 };

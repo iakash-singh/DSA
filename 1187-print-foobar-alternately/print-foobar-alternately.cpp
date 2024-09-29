bde2d2f77331e5c05994bd30ab9f21d05d914279
Akash Singh <77776282+iakash-singh@users.noreply.github.com>
2024-09-29 19:48:47 +0530

    Time: 17 ms (19.17%) | Memory: 10.4 MB (36.70%) - LeetSync

--- 1187-print-foobar-alternately/print-foobar-alternately.cpp
+++ 1187-print-foobar-alternately/print-foobar-alternately.cpp
@@ -1,39 +1,31 @@
 class FooBar {
 private:
     int n;
-    mutex mtx;
-    bool fooTurn = true;
+    atomic<bool> fooTurn;
 
 public:
     FooBar(int n) {
+        fooTurn = true;
         this->n = n;
     }
 
     void foo(function<void()> printFoo) {
         
         for (int i = 0; i < n; i++) {
-            while(true){
-                unique_lock<mutex> lock(mtx);
-                if(fooTurn){
-                    printFoo();
-                    fooTurn = false;
-                    break;
-                }
-            }
+            while(!fooTurn.load());
+        	printFoo();
+            fooTurn.store(false);
         }
     }
 
     void bar(function<void()> printBar) {
         
         for (int i = 0; i < n; i++) {
-            while(true){
-                unique_lock<mutex> lock(mtx);
-                if(!fooTurn){
-                    printBar();
-                    fooTurn = true;
-                    break;
-                }
-            }
+        	
+            while(fooTurn.load());
+        	printBar();
+            fooTurn.store(true);
+
         }
     }
 };

3c33bde6c2018cbaab45d35103af69dd9b2f91f2
Akash Singh <77776282+iakash-singh@users.noreply.github.com>
2024-10-02 00:40:19 +0530

    Time: 81 ms (87.96%) | Memory: 64.2 MB (96.96%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        vector<int> remainderCount(k, 0);
-        for (int num : arr) {
-            int remainder = ((num % k) + k) % k; 
-            remainderCount[remainder]++;
-        }
+        vector<int> reminder(k,0);
         
-        // Check pairs
-        if (remainderCount[0] % 2 != 0) { 
+        for(int num : arr){
+            int remi = ((num%k)+k)%k;
+            reminder[remi]++;
+        }
+
+        if(reminder[0]%2!=0){
             return false;
         }
-        
-        for (int i = 1; i <= k / 2; i++) {
-            if (remainderCount[i] != remainderCount[k - i]) { 
+        for(int i=1;i<=k/2;i++){
+            if(reminder[i]!=reminder[k-i]){
                 return false;
             }
         }
-        
+
         return true;
     }
 };

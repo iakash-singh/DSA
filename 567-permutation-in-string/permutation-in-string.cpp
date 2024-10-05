d2fe6f0c90607bef3ef83d5f98b2e3a4162c08cb
Akash Singh <77776282+iakash-singh@users.noreply.github.com>
2024-10-05 15:09:46 +0530

    Time: 10 ms (41.85%) | Memory: 8.7 MB (80.36%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n1 = s1.size(),n2=s2.size();
+        if(n1>n2) return false;
+
+        vector<int> count1(26,0),count2(26,0);
+        for(char c : s1){
+            count1[c-'a']++;
+        }
+
+        for(int i=0;i<n2;i++){
+            count2[s2[i]-'a']++;
+
+            if(i>=n1){
+                count2[s2[i-n1]-'a']--;
+            }
+
+            if(count1==count2){
+                return true;
+            }
+        }
+
+        return false;
+
+
+    }
+};

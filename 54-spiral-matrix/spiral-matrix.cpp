9283e52abcb90b3cdea39ca63279ebe4f906a22c
Akash Singh <77776282+iakash-singh@users.noreply.github.com>
2025-08-27 15:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (97.34%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -8,7 +8,7 @@ public:
         int top=0,left=0,bottom = n-1,right = m-1;
 
         while(top<=bottom && left<=right){
-            // left to right
+            
             for(int i=left;i<=right;i++){
                 ans.push_back(mat[top][i]);
             }
@@ -16,14 +16,14 @@ public:
             top++;
 
 
-            //top to botton
+            
             for(int i=top;i<=bottom;i++){
                 ans.push_back(mat[i][right]);
             }
 
             right--;
 
-            //right---->left
+            
             if(top<=bottom){
                 for(int i=right;i>=left;i--){
                     ans.push_back(mat[bottom][i]);

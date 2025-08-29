0bc862ce51647507a9b0839fa82884dfd751af8a
Akash Singh <77776282+iakash-singh@users.noreply.github.com>
2025-08-30 01:34:50 +0530

    Time: 51 ms (50.30%) | Memory: 29 MB (89.75%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,33 +5,32 @@ public:
         sort(nums.begin(),nums.end());
         int n = nums.size();
 
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue ;
+            }
 
-            int left = i+1;
-            int right = n-1;
+            int j = i+1;
+            int k = n-1;
 
-            while(left<right){
-                int sum = nums[i] + nums[left] + nums[right];
+            while(j<k){
+                int total = nums[i] + nums[j] + nums[k];
 
-                if(sum==0){
-                    ans.push_back({nums[i],nums[left],nums[right]});
-
-                    while(left<right && nums[left]==nums[left+1]) left++;
-                    while(left<right && nums[right]==nums[right-1]) right--;
-
-                    left++;
-                    right--;
+                if(total>0){
+                    k--;
                 }
-                else if(sum<0){
-                    left++;
+                else if(total<0){
+                    j++;
                 }
                 else{
-                    right--;
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    while(nums[j] == nums[j-1] && j<k){
+                        j++;
+                    }
                 }
             }
         }
-
         return ans;
     }
 };
